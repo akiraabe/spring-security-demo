db8745e625b497aef7354826f342c7c36683f277
Akira Abe <akiraabe2005@yahoo.co.jp>
2022-03-04 06:08:42 +0900

    implemented prime calc

--- complete/src/main/java/com/example/securingweb/SayController.java
+++ complete/src/main/java/com/example/securingweb/SayController.java
@@ -10,4 +10,19 @@ public class SayController {
     public String say() {
         return "say";
     }
+
+    @GetMapping("/prime")
+    public String prime() {
+        for (int i = 2; i < 50000000; i++) {
+            isPrime(i);
+        }
+        return "prime";
+    }
+
+    private boolean isPrime(int number) {
+        for (int i = 2; i*i <= number; i++) {
+            if (number % i == 0) return false;
+        }
+        return true;
+    }
 }
--- complete/src/main/java/com/example/securingweb/WebSecurityConfig.java
+++ complete/src/main/java/com/example/securingweb/WebSecurityConfig.java
@@ -29,9 +29,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.and()
 			.logout()
 				.permitAll()
-		;
-//        http.requiresChannel(channel ->channel
-//                .anyRequest().requiresSecure());
+				.and()
+				;
 	}
 
 	@Bean
